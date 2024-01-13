b5ba072de5bcf75eccc48d3da230db6265db3f28
patacca <patacca@autistici.org>
2024-01-13 17:56:16 +0100

    Add more abi::type_info derived classes

--- skald.cpp
+++ skald.cpp
@@ -17,11 +17,6 @@ using BinaryNinja::Structure;
 using BinaryNinja::StructureBuilder;
 using BinaryNinja::Type;
 
-std::array<std::string, 2> type_info_classes = {
-    "_ZTVN10__cxxabiv117__class_type_infoE",
-    "_ZTVN10__cxxabiv121__vmi_class_type_infoE",
-};
-
 Skald::Skald() { BinaryNinja::LogDebug("Initializing skald plugin"); }
 
 bool Skald::init() {
@@ -42,17 +37,12 @@ void Skald::run(BinaryNinja::BinaryView* view) {
     // Search for RTTI entry point by looking at the relocations
     for (auto [start, end] : view->GetRelocationRanges()) {
         for (const auto& rel : view->GetRelocationsAt(start)) {
-            // const BNRelocationInfo info = rel->GetInfo();
             const auto symbol = rel->GetSymbol();
 
             if (!symbol)  // No symbol for this relocation, just skip it
                 continue;
 
-            const std::string symbolName = symbol->GetRawName();
-
-            for (size_t i = 0; i < type_info_classes.size(); ++i)
-                if (symbolName == type_info_classes[i])
-                    this->parseRTTI(start, static_cast<TypeInfo>(i));
+            this->parseRTTI(start, symbol->GetRawName());
         }
     }
 
@@ -62,7 +52,12 @@ void Skald::run(BinaryNinja::BinaryView* view) {
         for (uint64_t addr : _view->GetDataReferences(rttiAddress)) {
             BinaryNinja::DataVariable var;
             _view->GetDataVariableAtAddress(addr, var);
-            BinaryNinja::LogInfo("variabl from %p -> %s", rttiAddress, var.type->GetString().c_str());
+
+            // RTTI type previously defined. Skip it
+            if (var.type->GetString().find("_class_type") != std::string::npos) continue;
+
+            BinaryNinja::LogInfo("variabl from %p -> %s (%p)", rttiAddress,
+                                 var.type->GetString().c_str(), addr);
         }
     }
 
@@ -126,20 +121,91 @@ Ref<Type> Skald::defineVmiClassType(uint32_t base_count) {
     return _view->GetTypeByName(typeName);
 }
 
-void Skald::parseRTTI(unsigned long address, const TypeInfo derivedType) {
+Ref<Type> Skald::defineSiClassType() {
+    QualifiedName typeName = QualifiedName("__si_class_type");
+    const auto type = _view->GetTypeByName(typeName);
+    if (!!type) return type;  // Define it once
+
+    StructureBuilder typeInfoBuilder;
+    typeInfoBuilder.AddMember(Type::PointerType(_view->GetDefaultArchitecture(), Type::VoidType()),
+                              "type_info");
+    typeInfoBuilder.AddMember(
+        Type::PointerType(_view->GetDefaultArchitecture(), Type::IntegerType(1, false, "char")),
+        "__type_name");
+    typeInfoBuilder.AddMember(Type::PointerType(_view->GetDefaultArchitecture(), Type::VoidType()),
+                              "__base_type");
+    Ref<Structure> typeInfoStruct = typeInfoBuilder.Finalize();
+    _view->DefineUserType(typeName, Type::StructureType(typeInfoStruct));
+
+    return _view->GetTypeByName(typeName);
+}
+
+Ref<Type> Skald::definePbaseClassType() {
+    QualifiedName typeName = QualifiedName("__pbase_class_type");
+    const auto type = _view->GetTypeByName(typeName);
+    if (!!type) return type;  // Define it once
+
+    StructureBuilder typeInfoBuilder;
+    typeInfoBuilder.AddMember(Type::PointerType(_view->GetDefaultArchitecture(), Type::VoidType()),
+                              "type_info");
+    typeInfoBuilder.AddMember(
+        Type::PointerType(_view->GetDefaultArchitecture(), Type::IntegerType(1, false, "char")),
+        "__type_name");
+    typeInfoBuilder.AddMember(Type::IntegerType(4, false, "unsigned int"), "__flags");
+    typeInfoBuilder.AddMember(Type::PointerType(_view->GetDefaultArchitecture(), Type::VoidType()),
+                              "__pointee");
+    Ref<Structure> typeInfoStruct = typeInfoBuilder.Finalize();
+    _view->DefineUserType(typeName, Type::StructureType(typeInfoStruct));
+
+    return _view->GetTypeByName(typeName);
+}
+
+void Skald::parseRTTI(unsigned long address, const std::string& symbolName) {
+    // Mangled names of type_info derived classes
+    using namespace std::literals;
+    static constexpr const std::array<std::pair<std::string_view, TypeInfo>, 6> type_info_classes{{
+        {"_ZTVN10__cxxabiv117__class_type_infoE"sv, TypeInfo::CLASS_TYPE_INFO},
+        {"_ZTVN10__cxxabiv121__vmi_class_type_infoE"sv, TypeInfo::VMI_CLASS_TYPE_INFO},
+        {"_ZTVN10__cxxabiv120__si_class_type_infoE"sv, TypeInfo::SI_CLASS_TYPE_INFO},
+        {"_ZTVN10__cxxabiv120__function_type_infoE"sv, TypeInfo::FUNCTION_TYPE_INFO},
+        {"_ZTVN10__cxxabiv119__pointer_type_infoE"sv, TypeInfo::POINTER_TYPE_INFO},
+        {"_ZTVN10__cxxabiv117__pbase_type_infoE"sv, TypeInfo::PBASE_TYPE_INFO},
+    }};
+
+    if (symbolName.find("_ZTVN10__cxxabiv1") == std::string::npos) return;  // Not a type_info class
+
+    TypeInfo derivedType = TypeInfo::UNSUPPORTED;
+    for (const auto& type_info : type_info_classes)
+        if (symbolName == type_info.first) derivedType = type_info.second;
+
     this->typeinfoClasses.push_back(address);
 
     switch (derivedType) {
-        case CLASS_TYPE_INF0: {
+        case CLASS_TYPE_INFO:
+        case FUNDAMENTAL_TYPE_INFO:
+        case ARRAY_TYPE_INFO:
+        case ENUM_TYPE_INFO:
+        case FUNCTION_TYPE_INFO: {
             const auto type = this->defineClassType();
             _view->DefineUserDataVariable(address, type->WithConfidence(0xff));
             break;
         }
-        case VMI_CLASS_TYPE_INF0: {
+        case VMI_CLASS_TYPE_INFO: {
             // Read uint32_t at address + 0x14, that's base_count
             uint32_t base_count;
             _view->Read(static_cast<void*>(&base_count), address + 0x14, 4);
             const auto type = this->defineVmiClassType(base_count);
+            _view->DefineUserDataVariable(address, type->WithConfidence(0xff));
+            break;
+        }
+        case SI_CLASS_TYPE_INFO: {
+            const auto type = this->defineSiClassType();
+            _view->DefineUserDataVariable(address, type->WithConfidence(0xff));
+            break;
+        }
+        case PBASE_TYPE_INFO:
+        case POINTER_TYPE_INFO: {
+            const auto type = this->definePbaseClassType();
             _view->DefineUserDataVariable(address, type->WithConfidence(0xff));
             break;
         }
@@ -159,10 +225,4 @@ BN_DECLARE_CORE_ABI_VERSION
 
 // Function run on plugin startup, do simple initialization here (Settings, BinaryViewTypes, etc)
 BINARYNINJAPLUGIN bool CorePluginInit() { return skald::Skald::get_instance().init(); }
-
-// (Optional) Function to add other plugin dependencies in case your plugin requires them
-// BINARYNINJAPLUGIN void CorePluginDependencies() {
-//     // For example, if you require the x86 to be loaded before your plugin
-//     AddRequiredPluginDependency("arch_x86");
-// }
 }
--- skald.h
+++ skald.h
@@ -7,8 +7,16 @@
 namespace skald {
 
 enum TypeInfo : uint32_t {
-    CLASS_TYPE_INF0,
-    VMI_CLASS_TYPE_INF0,
+    CLASS_TYPE_INFO,
+    VMI_CLASS_TYPE_INFO,
+    SI_CLASS_TYPE_INFO,
+    FUNCTION_TYPE_INFO,
+    PBASE_TYPE_INFO,
+    POINTER_TYPE_INFO,
+    FUNDAMENTAL_TYPE_INFO,
+    ARRAY_TYPE_INFO,
+    ENUM_TYPE_INFO,
+    UNSUPPORTED,
 };
 
 class Skald {
@@ -30,10 +38,12 @@ class Skald {
     std::vector<uint64_t> typeinfoClasses;  // Vector containing the address of each typeinfo class
 
     Skald();
-    void parseRTTI(unsigned long address, const TypeInfo derivedType);
+    void parseRTTI(unsigned long address, const std::string& symbolName);
     BinaryNinja::Ref<BinaryNinja::Type> defineClassType();
     BinaryNinja::Ref<BinaryNinja::Type> defineBaseClass();
     BinaryNinja::Ref<BinaryNinja::Type> defineVmiClassType(uint32_t base_count);
+    BinaryNinja::Ref<BinaryNinja::Type> defineSiClassType();
+    BinaryNinja::Ref<BinaryNinja::Type> definePbaseClassType();
 };
 
 }  // namespace skald
