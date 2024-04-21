ea20163493886035412f08419d1560bd71cdf428
patacca <patacca@autistici.org>
2024-04-22 01:24:06 +0200

    Refactor InheritanceGraph

--- inheritance_graph.cpp
+++ inheritance_graph.cpp
@@ -15,17 +15,17 @@ namespace skald {
 InheritanceGraph::InheritanceGraph() {}
 
 void InheritanceGraph::addNode(const std::string &name, address_t rttiAddress,
-                               const std::vector<address_t> &children) {
+                               const std::vector<edge_t> &children) {
     // Add the missing children
-    for (auto addr : children) {
+    for (const auto &[addr, e_flags] : children) {
         // First time adding the children node. Add it as a skeleton node that will be later
         // initialized
         if (!this->idMap.contains(addr)) {
             this->idMap[addr] = this->graph.size();
-            this->graph.push_back({{}, {}, "", addr});
+            this->graph.push_back({{}, {}, "", addr, addr});
         }
         this->roots.erase(addr);  // Children is not a root anymore
-        this->graph[this->idMap[addr]].parents.push_back(rttiAddress);
+        this->graph[this->idMap[addr]].parents.push_back({rttiAddress, e_flags});
     }
 
     // Add it as a leaf only if it has no children
@@ -37,17 +37,22 @@ void InheritanceGraph::addNode(const std::string &name, address_t rttiAddress,
         this->graph[this->idMap[rttiAddress]].name = name;
         this->graph[this->idMap[rttiAddress]].children = children;
     } else {  // First time adding it. Create the node
-        Node node{{}, children, name, rttiAddress};
+        Node node{{}, children, name, rttiAddress, rttiAddress};
         this->roots.insert(rttiAddress);
         this->idMap[rttiAddress] = this->graph.size();
         this->graph.push_back(std::move(node));
     }
 }
 
-Node &InheritanceGraph::getNode(address_t addr) {
-    if (!this->idMap.contains(addr))
-        throw std::invalid_argument(fmt::format("No node at address %p", addr));
-    return this->graph[this->idMap[addr]];
+Node &InheritanceGraph::getNodeById(const node_identifier_t &id) {
+    if (!this->idMap.contains(id))
+        throw std::invalid_argument(fmt::format("No node with id %p", id));
+    return this->graph[this->idMap[id]];
+}
+
+Node &InheritanceGraph::getNodeByAddr(const address_t &addr) {
+    // In this case the id is the same as the address but in future it might change
+    return this->getNodeById(addr);
 }
 
 }  // namespace skald
--- inheritance_graph.h
+++ inheritance_graph.h
@@ -1,37 +1,56 @@
 #pragma once
 
 #include <cstdint>
+#include <ranges>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
+#include "bitmask.h"
+
 namespace skald {
+
+typedef enum : uint32_t { VIRTUAL = 1, PUBLIC = 2 } EdgeFlag;
+DECLARE_BITMASK(EdgeFlag);
 
 typedef uint64_t address_t;
 typedef address_t node_identifier_t;  // A node identifier is also an address
+
+// Inheritance edge type. It has a node identifier and EdgeFlag describing the edge type
+typedef std::pair<node_identifier_t, EdgeFlag> edge_t;
 
 class Node {
    public:
     bool isLeaf() { return children.empty(); }
 
-    std::vector<node_identifier_t> parents;
-    std::vector<node_identifier_t> children;
+    std::vector<edge_t> parents;
+    std::vector<edge_t> children;
     std::string name;
     address_t rttiAddress;
+    node_identifier_t id;  // For now the address is the same as the id, but it might change
 };
 
 class InheritanceGraph {
    public:
     InheritanceGraph();
     void addNode(const std::string &name, address_t rttiAddress,
-                 const std::vector<address_t> &children);
-    Node &getNode(address_t addr);
+                 const std::vector<edge_t> &children);
+    Node &getNodeByAddr(const address_t &addr);
+    Node &getNodeById(const node_identifier_t &id);
+
+    auto getRoots() {
+        return std::views::transform(this->roots, [&](const node_identifier_t id) -> Node & {
+            return this->graph[this->idMap[id]];
+        });
+    };
 
    private:
     std::vector<Node> graph;
-    // Map from node identifier to index in the graph vector
+
+    // Map from a node identifier to its index in the graph vector
     std::unordered_map<node_identifier_t, uint32_t> idMap;
+
     std::unordered_set<node_identifier_t> leaves;
     std::unordered_set<node_identifier_t> roots;
 };
